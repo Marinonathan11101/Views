020846d0ba0bbb5a8a48f29ab7932a3804a8c8bf
Whitephantom666 <112992599+Whitephantom666@users.noreply.github.com>
2023-04-04 12:15:04 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
-<div>
-    <h1><?= $title ?>s</h1>
+<div class="jumbotron position-relative overflow-hidden p-3 p-md-5 m-md-3 bg-light">
+    <div class="row">
+        <div class="container col-md-6 p-lg-5 mx-auto my-5">
+            <h1 class="display-3">Helpets</h1>
+            <p class="lead font-weight-normal">Help us help furry friends find the loving homes they deserve. Check out our animals or schedule a time to come meet your future fur-riend for life!</p>
+            <p><a class="btn btn-primary btn-lg" href="<?= ROOT_PATH ?>/resources/list" role="button">Adopt Now &raquo;</a></p>
+        </div>
+
+        <div class="col-md-4 d-flex p-2">
+            <img src="assets\thomas-park-On6bRQRn5lY-unsplash.jpg" alt="" style="max-width: 600px; height: auto;">
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row mb-2">
+        <div class="col-md-8">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static" style="height: 250px !important;">
+                    <strong class="d-inline-block mb-2 text-primary">Animals</strong>
+                    <h3 class="mb-0">Looking to adopt?</h3>
+                    <p class="card-text mb-auto">Find your new furry friend at our animal shelter! Click here to view our adoptable dogs, cats, and other pets waiting for their forever homes.</p>
+                    <a href="<?= ROOT_PATH ?>/resources" class="card-link ms-auto">See More &raquo;</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="assets\thomas-park-On6bRQRn5lY-unsplash.jpg" width="350" height="250" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static" style="height: 250px !important;">
+                    <strong class="d-inline-block mb-2 text-primary">Contact</strong>
+                    <h3 class="mb-0">Make an appointment</h3>
+                    <p class="mb-auto">Please don't hesitate to get in touch with us. We are always happy to answer any questions you may have and help you find your new best fur-riend.</p>
+                    <a href="#" class="card-link ms-auto">Go &raquo;</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
